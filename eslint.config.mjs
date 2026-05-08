83665432730512c331d6ba909593ec419bc132a8
smmariquit <smmariquit@up.edu.ph>
2026-05-09 00:12:28 +0800

    chore(lint): add ESLint flat config with Next.js rules

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import nextConfig from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+const config = [
+  {
+    ignores: [".next/**", "node_modules/**"],
+  },
+  ...nextConfig,
+  ...nextCoreWebVitals,
+];
+
+export default config;
